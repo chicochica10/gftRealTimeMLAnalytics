dbb53c7e01b05dfe3db00237a9791a5aed56c525
chicochica10 <chicochica10@gmail.com>
2015-05-01 13:20:16 +0200

    sbt fix

--- src/main/scala/com/gft/realtimemlanalytics/WordCount.scala
+++ src/main/scala/com/gft/realtimemlanalytics/WordCount.scala
@@ -20,5 +20,6 @@ object WordCount {
       val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
       // Save the word count back out to a text file, causing evaluation.
       counts.saveAsTextFile(outputFile)
+      
     }
 }
