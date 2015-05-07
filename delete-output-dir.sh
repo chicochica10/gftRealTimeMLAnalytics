05586ca4d860c6e81588a8eeb0f7297b39e0932c
chicochica10 <chicochica10@gmail.com>
2015-05-07 14:38:16 +0200

    delete output directory script

--- delete-output-dir.sh
+++ delete-output-dir.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-outputdir= $1
+if [ -z "$1" ]
+	then
+    		echo "No output dir supplied"
+  	else 
+    		if [ ! -d "$1" ]; then
+				echo "$1 dir doesn't exist"
+    		else
+			echo "removing $1..."
+			rm -rf $1
+			echo "$1 removed!"
+		fi
+fi
 
-echo ${outputdir}
